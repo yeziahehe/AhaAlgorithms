bc1a492bf72300b142189b6124fb2e59c8de0701
叶帆 <yeziahehe@gmail.com>
2015-12-24 16:11:49 +0800

    Initial Commit

---
+++ AhaAlgorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AhaAlgorithms
+//
+//  Created by 叶帆 on 15/12/24.
+//  Copyright © 2015年 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
