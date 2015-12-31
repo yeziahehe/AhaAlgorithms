2a3f38f5ed4eeb7f8e47723ee295bb1a7c0e45c6
叶帆 <yeziahehe@gmail.com>
2015-12-31 17:49:44 +0800

    Quicksort

---
+++ 1_3_QuickSort/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  1_3_QuickSort
+//
+//  Created by 叶帆 on 15/12/25.
+//  Copyright © 2015年 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ *  快速排序 (Quick Sort)
+ *  原理：利用二分法的思想，找到基准数，每一轮将这一轮的基准数归位，直到所有的数都归位为止。
+ *  实现：利用左右哨兵，默认从大到小排列的，基准数在哪边就让哪边的哨兵先出发。
+ *  双层嵌套循环，时间复杂度 O(NLogN)。
+ *  在算法题中使用较多，是最常用的排序方法，建议能够理解并牢记，手写和代码都实现一遍。
+ */
+#include <stdio.h>
+
+void sort(int *a, int left, int right) {
+    
+}
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
